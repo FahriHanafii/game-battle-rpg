13fc9256d2e1d4049daa355720d94989b651a8de
FahriHanafii <168062332+FahriHanafii@users.noreply.github.com>
2024-04-25 12:48:47 +0700

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,14 @@
+public class Item {
+    // Implementasi atribut dan metode khusus untuk item jika diperlukan,Misalnya, implementasi untuk jenis-jenis item tertentu
+}
+class Elixir extends Item {
+    
+}
+
+class Ether extends Item{
+    
+}
+
+class Potion extends Item {
+    
+}
---
+++ Weapon.java
@@ -0,0 +1,12 @@
+public class Weapon {
+    private int AP; // Attack Point
+
+    public Weapon(String type, int AP) {
+        this.AP = AP;
+    }
+
+    // Getter dan setter
+    public int getAP() {
+        return AP;
+    }
+}
